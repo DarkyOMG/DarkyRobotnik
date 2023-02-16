01ca40f830823b25b83d426c81e145fa4ac66ad7
Alex Brühl <alexbruehl@gmx.de>
2023-02-16 21:02:09 +0100

    fixed riddlemap

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const tmi = require('tmi.js');
 const fs = require('fs');
 const exec = require('child_process').exec;
 const { getAudioDurationInSeconds } = require('get-audio-duration')
-const riddlemap = require('./utils/riddlemap');
 
 
 
@@ -77,7 +76,24 @@ standardmap = {
       }
     }
 }
-
+const riddlemap = {
+  "!riddle":
+    "104 116 116 112 115 058 047 047 119 119 119 046 121 111 117 116 117 098 101 046 099 111 109 047 119 097 116 099 104 063 118 061 100 086 098 053 080 102 112 109 119 073 069",
+  "!0765":
+    "Zhofkhq Lqwhusuhwhq pxvv vlfk RWEW riw yrq Doha jhidoohq odvvhq?",
+  "!eminem":
+    "CompSys - The Game ist ein echt cooles Spiel... Aber was ist der Titel?",
+  "!konzeption und implementierung von videospielen zur lernunterstützung in unity3d":
+    "Welche Sprache spricht eigentlich Gilly?",
+  "!ruby":
+    "Wiki ist ein Wikinger.. Oder so.. Oder was anderes? Hauptsache heiß!.. Aber wie heiß nun eigentlich?",
+  "!superhot":
+    "No spaces. All lowercase. No '+'.\n Max Lieblingsspiel + OTBT-Chef-Vorname + BA-Betreuer-vorname + Gillys Lieblingsspiel + Wiki-Artikel-last-editor",
+  "!maplestoryjenstimhangmandobiko":
+      "Du hast gewonnen!",
+  "!firstwinner":
+  "@pinkfluffyfluffycorn hat das Rätsel als erstes gelöst und hat sich damit einen 10€-Steam-Gutschein verdient :)"
+}
 commandmap = {
   "!reloadcommands":
     (target, context, msg, self) => {
